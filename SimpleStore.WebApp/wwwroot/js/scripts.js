a1dcdd2c9cf2061f0ea3a36594bcb1cb756d5c35
nick4real <67540024+nick4real@users.noreply.github.com>
2025-11-11 21:24:32 +0100

    Web app setup

---
+++ SimpleStore.WebApp/wwwroot/js/scripts.js
@@ -0,0 +1,12 @@
+﻿window.navbarToggle = (navbarId, isShow) => {
+    if (isShow) {
+        document.getElementById(navbarId).style.display = "flex";
+    }
+    else {
+        document.getElementById(navbarId).style.display = "none";
+    }
+}
+
+window.navbarClose = (navbarId) => {
+    document.getElementById(navbarId).style.display = "none";
+}
